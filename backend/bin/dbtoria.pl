7b6416342f68eec49ebf5076f6b84f81ac2ffd9c
Matthias Bloch <matthias.bloch@puffin.ch>
2014-04-11 18:55:21 +0200

    dbtoria.pl line for DBI under perl in thirdparty/ added

--- backend/bin/dbtoria.pl
+++ backend/bin/dbtoria.pl
@@ -4,6 +4,7 @@ use warnings;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 use lib "$FindBin::Bin/../../thirdparty/lib/perl5";
+use lib "$FindBin::Bin/../../thirdparty/lib/perl5/x86_64-linux-thread-multi";
 use Mojolicious::Commands;
 use DbToRia::MojoApp;
 

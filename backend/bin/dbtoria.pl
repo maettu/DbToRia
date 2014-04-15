c24b3d302e15ff051e3bf2a61487e77bed3adf64
Matthias Bloch <matthias.bloch@puffin.ch>
2014-04-15 07:23:23 +0200

    dbtoria.pl takes source / debug command line option

--- backend/bin/dbtoria.pl
+++ backend/bin/dbtoria.pl
@@ -10,5 +10,13 @@ use DbToRia::MojoApp;
 
 $ENV{MOJO_APP} = DbToRia::MojoApp->new;
 
+# enable debugging
+my $mode = $ARGV[1] // '';
+if ($mode eq 'source' or $mode eq 'debug'){
+    print "***\nturning on source / debug mode\n***\n";
+    $ENV{QX_SRC_MODE} = 1;
+    $ENV{QX_SRC_PATH} = "$FindBin::Bin/../../frontend";
+}
+
 # Start commands
 Mojolicious::Commands->start_app('DbToRia::MojoApp');
